61fa74e9fb4f647445b1feec5c896a57a8c27866
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-08-02 20:50:58 +0300

    Add expired products collector launcher

--- Bot/db_aps.py
+++ Bot/db_aps.py
@@ -1,5 +1,7 @@
+from asyncio import sleep
 import logging
 import os
+from random import randint
 import typing
 
 import redis
@@ -44,29 +46,47 @@ def store_watched_product_info(product_info: dict) -> None:
     db.hmset('{}{}'.format(DB_PRODUCT_PREFIX, product_info['product_id']), product_info)
 
 
-def find_expired_products() -> None:
-    '''Find expired products in db'''
+def collect_searches() -> dict:
+    '''Collect all existing searches from db'''
     db = get_database_connection()
+    db_keys = [key.decode('utf-8') for key in db.keys()]
+    search_keys = [key for key in db_keys if key.startswith(DB_SEARCH_PREFIX)]
+    searches = {}
+    for key in search_keys:
+        user_id = key.split(':')[-1]
+        user_searches = [search_url.decode('utf-8') for search_url in db.hvals(key)]
+        searches[user_id] = user_searches
+    return searches
 
+
+async def run_expired_products_collector(sleep_time=43200):
+    '''Runs collector witch remove expired products from db'''
+    while True:
+        await find_expired_products()
+        await sleep(sleep_time)
+
+
+async def find_expired_products() -> None:
+    '''Find and remove expired products from db'''
+    db = get_database_connection()
     existing_keys = db.keys()
-    product_keys = []
-    for key in existing_keys:
-        if DB_PRODUCT_PREFIX.encode() in key:
-            product_keys.append(key)
 
+    product_keys = [key for key in existing_keys if DB_PRODUCT_PREFIX.encode() in key]
     expired_keys = []
     for key in product_keys:
         if _is_expired(key):
             expired_keys.append(key)
+        await sleep(randint(10, 20))
+
     if expired_keys:
         db.delete(*expired_keys)
 
 
-def _is_expired(key: str) -> typing.Optional[bool]:
+def _is_expired(product_key: str) -> typing.Optional[bool]:
     '''Get product page and check for expiration selectors in it'''
     db = get_database_connection()
     expired_selectors = ['item-closed-warning', 'item-view-warning-content']
-    product_url = db.hgetall(key)[b'product_url'].decode('utf-8')
+    product_url = db.hget(product_key, 'product_url').decode('utf-8')
     response = requests.get(product_url)
     response.raise_for_status()
     text = response.text
