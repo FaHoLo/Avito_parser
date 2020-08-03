a3b36e2132d98898e087d1da73ea010ce37440f5
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-08-03 13:33:28 +0300

    Add bot launcher

---
+++ Bot/start_bot.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+
+from avito_parser import start_parser
+from db_aps import run_expired_products_collector
+from tg_bot import bot, dispatcher, executor
+from utils import handle_exception
+
+
+def main():
+    load_dotenv()
+    while True:
+        try:
+            start_bot()
+        except Exception:
+            handle_exception('main_start_bot_logger')
+
+
+def start_bot():
+    parser_sleep_time = 1800
+    collector_sleep_time = 43200  # 12 hours
+    dispatcher.loop.create_task(start_parser(bot, parser_sleep_time))
+    dispatcher.loop.create_task(run_expired_products_collector(collector_sleep_time))
+    executor.start_polling(dispatcher)
+
+
+if __name__ == '__main__':
+    main()
